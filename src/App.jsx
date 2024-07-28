9cad1b254dcb45676805795454acd4a5a6a165be
siddesh patil <siddeshp388@gmail.com>
2024-07-28 14:46:32 +0530

    initial commits

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ImageViewer from "./components/imageViewer/ImageViewer";
+
+function App() {
+  return (
+    <>
+      <ImageViewer />
+    </>
+  );
+}
+
+export default App;
